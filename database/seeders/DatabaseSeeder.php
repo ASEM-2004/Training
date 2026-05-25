7136a9fa6e694021b4b3c3272f36993b839056c5
ASEM-2004 <asem.motasem2004@gmial.com>
2026-05-25 15:15:16 +0300

    first upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->count(5)->has(
+        Post::factory()->count(3)->has(
+        Comment::factory()->count(4) 
+        )
+        )->create();
+    }
+}
